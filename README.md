d9226ed575744a69ac4c163e379866fe37c7023b
chornokozynskyi <kyrylo.chornokozynskyi@ukma.edu.ua>
2023-06-06 21:07:04 +0300

    Write a program that builds a dictionary of terms based on a given collection of text files.
    Text files can be submitted in any format.
    The size of text files is at least 150 K.
    The number of text files is at least 10.
    Glossary of terms to save to disk.
    Estimate the size of the collection, the total number of words in the collection and the size of the dictionary.
    Justify the data structure
    Evaluate the complexity of the algorithm
    Try several formats of saving the dictionary (dictionary serialization, saving to a text file, etc.) and compare the results.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # hs-information-retrieval
+
+Welcome to the Information Retrieval project repository! This project is written in Haskell and is part of the University Course "Information Retrieval." 
+
+## About
+
+The aim of this project is to implement advanced algorithms and techniques for information retrieval. Through this repository, we explore the fascinating world of retrieving and analyzing information using Haskell's powerful and expressive features.
+
+## Features
+
+- Efficient algorithms for retrieving information
+- Analysis and processing of retrieved data
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,59 @@
-module Main (main) where
+import System.Directory
+import System.IO
+import Text.Regex
+import qualified Data.Text as T
+import qualified Data.Set as Set
+import Text.Regex.TDFA ((=~), getAllTextMatches, AllTextMatches)
+
+trim :: String -> String
+trim = T.unpack . T.strip . T.pack
+
+getFileNames :: FilePath -> IO [FilePath]
+getFileNames dir = do
+    allNames <- getDirectoryContents dir
+    return $ filter (`notElem` [".", ".."]) allNames
+
+readFileWithEncoding :: FilePath -> IO String
+readFileWithEncoding filePath = do
+  h <- openFile filePath ReadMode
+  hSetEncoding h latin1
+  content <- hGetContents h
+  return $ processFile content
+
+processFile :: String -> String
+processFile str = 
+    let noTags = subRegex (mkRegex "<[^>]*>") str ""
+        noSpecialChars = subRegex (mkRegex "[^a-zA-Z0-9 ,.;:'`\"!?]") noTags "" :: String
+    in trim noSpecialChars
+
+writeStringToFile :: FilePath -> String -> IO ()
+writeStringToFile filePath contents = do
+  h <- openFile filePath WriteMode
+  hSetEncoding h utf8
+  hPutStr h contents
+  hClose h
+
+writeSetToFile :: FilePath -> Set.Set String -> IO ()
+writeSetToFile filePath set = do
+  h <- openFile filePath WriteMode
+  hSetEncoding h utf8
+  hPutStr h (show set)
+  hClose h
+
+getWords :: String -> [String]
+getWords str = getAllTextMatches (str =~ "[a-zA-Z0-9']+" :: AllTextMatches [] String)
+
+uniqueWords :: [String] -> Set.Set String
+uniqueWords contents = foldl (\acc content -> acc `Set.union` Set.fromList (getWords content)) Set.empty contents
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    let folder = "resources/fb2"
+    fileNames <- getFileNames folder
+    contents <- mapM readFileWithEncoding $ map ((folder ++ "/") ++) fileNames
+    let fileContentPairs = zip fileNames contents
+    let wordsSet = uniqueWords contents
+    mapM_ (\(fileName, content) -> writeStringToFile ("resources/out/" ++ fileName) content) fileContentPairs
+    writeSetToFile "resources/out/set.txt" wordsSet 
+    print "done"
+
